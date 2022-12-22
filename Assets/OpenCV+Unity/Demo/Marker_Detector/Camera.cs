75908624f6238e663444dc212b3472cad570f680
vQTarathiel <omzata97@gmail.com>
2022-12-22 23:31:27 +0300

    Padding eklendi

--- Assets/OpenCV+Unity/Demo/Marker_Detector/Camera.cs
+++ Assets/OpenCV+Unity/Demo/Marker_Detector/Camera.cs
@@ -1,8 +1,7 @@
+
 using OpenCvSharp;
 using OpenCvSharp.Aruco;
 using OpenCvSharp.Demo;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -12,46 +11,49 @@ public class Camera : WebCamera
     protected override bool ProcessTexture(WebCamTexture input, ref Texture2D output)
     {
         image = OpenCvSharp.Unity.TextureToMat(input);
-        
+        QRGeneration.GenerateQR();
+
         // Sik sik Marker tanýma Þeysileri
 
-        if(output == null)
+        if (output == null)
         {
             output = OpenCvSharp.Unity.MatToTexture(image);
-			QRGeneration.GenerateQR();
-		}
+        }
         else
         {
-			// Create default parameres for detection
-			DetectorParameters detectorParameters = DetectorParameters.Create();
+            // Create default parameres for detection
+            DetectorParameters detectorParameters = DetectorParameters.Create();
 
-			// Dictionary holds set of all available markers
-			Dictionary dictionary = CvAruco.GetPredefinedDictionary(PredefinedDictionaryName.DictArucoOriginal);
+            // Dictionary holds set of all available markers
+            Dictionary dictionary = CvAruco.GetPredefinedDictionary(PredefinedDictionaryName.DictArucoOriginal);
 
-			// Variables to hold results
-			Point2f[][] corners;
-			int[] ids;
-			Point2f[][] rejectedImgPoints;
+            // Variables to hold results
+            Point2f[][] corners;
+            int[] ids;
+            Point2f[][] rejectedImgPoints;
 
-			// Create Opencv image from unity texture
-			Mat mat = image;
+            // Create Opencv image from unity texture
+            Mat mat = image;
 
-			// Convert image to grasyscale
-			Mat grayMat = new Mat();
-			Cv2.CvtColor(mat, grayMat, ColorConversionCodes.BGR2GRAY);
+            // Convert image to grasyscale
+            Mat grayMat = new Mat();
+            Cv2.CvtColor(mat, grayMat, ColorConversionCodes.BGR2GRAY);
 
-			// Detect and draw markers
-			CvAruco.DetectMarkers(grayMat, dictionary, out corners, out ids, detectorParameters, out rejectedImgPoints);
-			CvAruco.DrawDetectedMarkers(mat, corners, ids);
+            // Detect and draw markers
+            CvAruco.DetectMarkers(grayMat, dictionary, out corners, out ids, detectorParameters, out rejectedImgPoints);
+            CvAruco.DrawDetectedMarkers(mat, corners, ids);
 
-			// Create Unity output texture with detected markers
-			Texture2D outputTexture = OpenCvSharp.Unity.MatToTexture(mat);
+            // Create Unity output texture with detected markers
+            Texture2D outputTexture = OpenCvSharp.Unity.MatToTexture(mat);
 
-			// Set texture to see the result
-			RawImage rawImage = gameObject.GetComponent<RawImage>();
-			rawImage.texture = outputTexture;
-			OpenCvSharp.Unity.MatToTexture(mat, output);
-		}
+            // Set texture to see the result
+            RawImage rawImage = gameObject.GetComponent<RawImage>();
+            rawImage.texture = outputTexture;
+
+            OpenCvSharp.Unity.MatToTexture(mat, output);
+
+            QRGeneration.GeneratePaddedImage(output);
+        }
 
         return true;
     }
--- Assets/OpenCV+Unity/Demo/Marker_Detector/QRGeneration.cs
+++ Assets/OpenCV+Unity/Demo/Marker_Detector/QRGeneration.cs
@@ -18,26 +18,53 @@ namespace OpenCvSharp.Demo
         {
             // Uygulamamýzý Çalaný Sikeriz Bu yüzden üst 2 QR kodu 34 06 yaptýk aq (pis siqerler)
             Dictionary dictionary = CvAruco.GetPredefinedDictionary(PredefinedDictionaryName.DictArucoOriginal);
-            Mat Identifier1 = new Mat();
-            Mat Identifier2 = new Mat();
+            Mat UstSolQR = new();
+            Mat UstSagQR = new();
 
-            CvAruco.DrawMarker(dictionary, 34, 50, Identifier1);
-            CvAruco.DrawMarker(dictionary, 06, 50, Identifier2);
+            CvAruco.DrawMarker(dictionary, 34, 50, UstSolQR);
+            CvAruco.DrawMarker(dictionary, 06, 50, UstSagQR);
 
             //Alttaki 2 QR kod ise albümün QR kodlarý olacak burda SQL kullanacaz
-            Mat AlbumSolAltQR = new Mat();
-            Mat AlbumSagAltQR = new Mat();
+            Mat AlbumSolAltQR = new();
+            Mat AlbumSagAltQR = new();
 
             var id1 = 293;  //TODO: Veri Tabanýndan alýnan ID atanacak
             var id2 = 31;   //TODO: Veri Tabanýndan alýnan ID atanacak
 
-            CvAruco.DrawMarker(dictionary, id1, 50, AlbumSolAltQR);
-            CvAruco.DrawMarker(dictionary, id2, 50, AlbumSagAltQR);
+            //CvAruco.DrawMarker(dictionary, id1, 50, AlbumSolAltQR);
+            //CvAruco.DrawMarker(dictionary, id2, 50, AlbumSagAltQR);
 
-            Mat abc = new Mat();
-            Cv2.Add(AlbumSolAltQR, AlbumSagAltQR, abc);
+            //ImageSaving(Unity.MatToTexture(UstSolQR));
+            //ImageSaving(Unity.MatToTexture(UstSagQR));
+            //ImageSaving(Unity.MatToTexture(AlbumSolAltQR));
+            //ImageSaving(Unity.MatToTexture(AlbumSagAltQR));
+        }
+        public static void GeneratePaddedImage(Texture2D input)
+        {
+            var PaddedImage = input.Clone();
 
-            ImageSaving(Unity.MatToTexture(abc));
+            PaddedImage.Reinitialize(input.width + 100, input.height + 100);
+            
+            var borderColor = new UnityEngine.Color(1, 1, 1, 1);
+            var borderWidth = 50;
+
+            for (var x = 0; x < PaddedImage.width; x++) {
+                for (var y = 0; y < PaddedImage.height; y++) {
+                    if (x < borderWidth || x > PaddedImage.width - 1 - borderWidth) PaddedImage.SetPixel(x, y, borderColor);
+                    else if (y < borderWidth || y > PaddedImage.height - 1 - borderWidth) PaddedImage.SetPixel(x, y, borderColor);
+                    else
+                    {
+                        PaddedImage.SetPixel(x, y, input.GetPixel(x - borderWidth, y - borderWidth));
+                    }
+                }
+            }
+
+            PaddedImage.Apply();
+
+            //PaddedImage.ApplyPadding(PaddingMode.Diagonal);
+            //PaddedImage.Apply();
+
+            ImageSaving(PaddedImage);
         }
         private static void ImageSaving(Texture2D input)
         {
